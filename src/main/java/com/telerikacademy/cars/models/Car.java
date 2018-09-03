37d1d02dff56497ede7d14095b5e45ed84ba8fda
Slavi <slavi_14@abv.bg>
2018-09-03 18:13:02 +0300

    second commit

--- src/main/java/com/telerikacademy/cars/models/Car.java
+++ src/main/java/com/telerikacademy/cars/models/Car.java
@@ -3,31 +3,53 @@ package com.telerikacademy.cars.models;
 import com.telerikacademy.cars.models.base.ModelBase;
 
 public class Car extends ModelBase {
-    private String name;
-    private String secretIdentity;
+    private String make;
+    private String carModel;
+    private int power;
+    private int cubicCapacity;
+
 
     public Car() {
 
     }
 
-    public Car(String name, String secretIdentity) {
-        setName(name);
-        setSecretIdentity(secretIdentity);
+    public Car(String make, String carModel, int power, int cubicCapacity) {
+        this.make = make;
+        this.carModel = carModel;
+        this.power = power;
+        this.cubicCapacity = cubicCapacity;
     }
 
-    public String getName() {
-        return name;
+    public String getMake() {
+        return make;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getCarModel() {
+        return carModel;
     }
 
-    public String getSecretIdentity() {
-        return secretIdentity;
+    public int getPower() {
+        return power;
     }
 
-    public void setSecretIdentity(String secretIdentity) {
-        this.secretIdentity = secretIdentity;
+    public int getCubicCapacity() {
+        return cubicCapacity;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public void setCubicCapacity(int cubicCapacity) {
+        this.cubicCapacity = cubicCapacity;
     }
 }
+
