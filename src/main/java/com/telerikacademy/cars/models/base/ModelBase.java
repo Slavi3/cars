3501a3f2d0ce99eef865ddf5d7fc75a88d3cd3d9
Slavi <slavi_14@abv.bg>
2018-09-04 15:02:14 +0300

    first commit

---
+++ src/main/java/com/telerikacademy/cars/models/base/ModelBase.java
@@ -0,0 +1,13 @@
+package com.telerikacademy.cars.models.base;
+
+public class ModelBase {
+    private int id;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
